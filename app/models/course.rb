e32f1366eb9ad08e9f818b874ec995953152daf5
Shamark Adgerson <adgerson11@gmail.com>
2019-08-21 16:13:28 +0000

    Course Validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,11 @@ class Course < ApplicationRecord
 # associations between our users and courses
 
     belongs_to :user
+
+# this validates that a course has a appropriate information upon creation.  
+
+    validates :title, presence: true
+    validates :description, presence: true
+# will validate that the cost is greater than 0. 
+    validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
