a3ea97c7bcebf655cceafb82a830a2bd3b631244
Shamark Adgerson <adgerson11@gmail.com>
2019-09-03 14:28:10 +0000

    Lesson 28- Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,9 +1,17 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user! 
+    before_action :require_authorized_for_current_lesson, only: [:current_user]
 
     def show
     end
 
     private 
+
+    def require_authorized_for_current_lesson
+        if !current_user.enrolled_in?(current_lesson.section.course) && current_lesson.section.course.user != current_user
+            redirect_to course_path(current_lesson.section.course), alert: 'You are not Authorized'
+        end
+    end
 
     helper_method :current_lesson
     def current_lesson
