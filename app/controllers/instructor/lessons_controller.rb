2a12c9d3b9ddda3351a7edd300e44acbcd3250e4
Shamark Adgerson <adgerson11@gmail.com>
2019-09-10 13:35:15 +0000

    L:36 New Lesson Form Modal

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,12 +1,8 @@
 class Instructor::LessonsController < ApplicationController
     before_action :authenticate_user!
-    before_action :require_authorized_for_current_section, only: [:new, :create]
+    before_action :require_authorized_for_current_section, only: [:create]
     before_action :require_authorized_for_current_lesson, only: [:update]
 
-# load section that's referenced by URL. 
-    def new
-        @lesson = Lesson.new
-    end
 # associate the create button with lessons and sections. 
     def create
         @lesson = current_section.lessons.create(lesson_params)
